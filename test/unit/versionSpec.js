779fd39e79f47c12413b325c9a090861a6b4399a
Dan Shreim <dan@snapjay.com>
2014-09-10 14:53:28 -0400

    Set up Travis

--- test/unit/versionSpec.js
+++ test/unit/versionSpec.js
@@ -1,12 +1,31 @@
 describe('version', function() {
     beforeEach(module('ngCart'));
 
-    it('should return current cow', inject(function(version) {
+    it('should return current 0.0.1-rc.2', inject(function(version) {
+        expect(version).toEqual('0.0.1-rc.2');
+    }));
+
+
+    it('should return current 0.0.1-rc.2', inject(function(version) {
+        expect(version).toEqual('0.0.1-rc.2');
+    }));
+
+
+    it('should return current 0.0.1-rc.2', inject(function() {
         expect('cow').toEqual('cow');
     }));
 
-    it('should return current key', inject(function(key) {
-        expect(key).toEqual('test');
+
+
+
+    it('should return current 0.0.1-rc.2', inject(function() {
+        expect(4+6).toEqual(10);
     }));
+
+
+    it('should return current 0.0.1-rc.2', inject(function() {
+        expect(5+5+9).toEqual(19);
+    }));
+
 
 });
